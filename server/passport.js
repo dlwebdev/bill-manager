2dafb360749839224f71c1d9486ca51e675f3660
Justin Weber <jjweber@fullsail.edu>
2017-01-28 21:50:08 -0500

    Updated passport for new creds

--- server/passport.js
+++ server/passport.js
@@ -4,9 +4,15 @@ const BitbucketStrategy = require('passport-bitbucket').Strategy;
 //module.exports = function(passport) {
 
   passport.use(new BitbucketStrategy({
-      consumerKey: '95YJCPZVaAHuCpUw66',
-      consumerSecret: 'GsDYSbrww7x9qsh3SFnYHapHjMTNjHTK',
-      callbackURL: "http://localhost:3000/auth/bitbucket/callback"
+      // FOR HEROKU
+      consumerKey: 'KXdEjPnxCESujBtC2U',
+      consumerSecret: '4bQTwXXR9cpZ3p9EDvps6W7G8qc2E2SE',
+      callbackURL: "https://weber-justin-bms.herokuapp.com/callback",
+
+      // FOR LOCALHOST
+      //consumerKey: '95YJCPZVaAHuCpUw66',
+      //onsumerSecret: 'GsDYSbrww7x9qsh3SFnYHapHjMTNjHTK',
+      //callbackURL: "http://localhost:3000/auth/bitbucket/callback"
     },
     function(token, tokenSecret, profile, cb) {
       User.findOrCreate({ bitbucketId: profile.username, displayname: profile.displayname, daysBeforeNotice: 7 }, function (err, user) {
